99874b6f5214d497c4358765701a1df4a9fe954e
Lindsay Villegas Licoa <villegas.licoa@gmail.com>
2023-11-16 00:38:44 -0500

    avance diseño

--- src/components/Colaboradores.jsx
+++ src/components/Colaboradores.jsx
@@ -1,8 +1,91 @@
-const Colaboradores = () => {
+import { Box, Card, CardContent, CardMedia, Typography, List, ListItem } from '@mui/material';
 
-    return(
-        <h1>COLABORADORES</h1>
-    )
+const colaboradores = [
+    {
+        nombre: "Lindsay Villegas",
+        imagen: "/ruta/a/lindsay.jpg", // Cambia esto por la ruta real de la imagen
+        logros: ["Logro 1", "Logro 2", "Logro 3"]
+    },
+    {
+        nombre: "Elías Cando",
+        imagen: "/ruta/a/elias.jpg", // Cambia esto por la ruta real de la imagen
+        logros: ["Logro 1", "Logro 2", "Logro 3"]
+    },
+    {
+        nombre: "Lindsay Villegas",
+        imagen: "/ruta/a/lindsay.jpg", // Cambia esto por la ruta real de la imagen
+        logros: ["Logro 1", "Logro 2", "Logro 3"]
+    },
+    {
+        nombre: "Elías Cando",
+        imagen: "/ruta/a/elias.jpg", // Cambia esto por la ruta real de la imagen
+        logros: ["Logro 1", "Logro 2", "Logro 3"]
+    },
+    {
+        nombre: "Lindsay Villegas",
+        imagen: "/ruta/a/lindsay.jpg", // Cambia esto por la ruta real de la imagen
+        logros: ["Logro 1", "Logro 2", "Logro 3"]
+    }
+];
+
+function Colaboradores() {
+    return (
+        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 2 }}>
+            {/* Fila superior */}
+            <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+                {colaboradores.slice(0, 3).map((colaborador, index) => (
+                    <Card key={index} sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={colaborador.imagen}
+                            alt={`Imagen de ${colaborador.nombre}`}
+                            sx={{ borderRadius: '50%' }}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {colaborador.nombre}
+                            </Typography>
+                            <List>
+                                {colaborador.logros.map((logro, idx) => (
+                                    <ListItem key={idx}>
+                                        {logro}
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </CardContent>
+                    </Card>
+                ))}
+            </Box>
+
+            {/* Fila inferior */}
+            <Box sx={{ display: 'flex', justifyContent: 'center', gap: 2 }}>
+                {colaboradores.slice(3).map((colaborador, index) => (
+                    <Card key={index} sx={{ maxWidth: 345 }}>
+                        <CardMedia
+                            component="img"
+                            height="140"
+                            image={colaborador.imagen}
+                            alt={`Imagen de ${colaborador.nombre}`}
+                            sx={{ borderRadius: '50%' }}
+                        />
+                        <CardContent>
+                            <Typography gutterBottom variant="h5" component="div">
+                                {colaborador.nombre}
+                            </Typography>
+                            <List>
+                                {colaborador.logros.map((logro, idx) => (
+                                    <ListItem key={idx}>
+                                        {logro}
+                                    </ListItem>
+                                ))}
+                            </List>
+                        </CardContent>
+                    </Card>
+                ))}
+            </Box>
+        </Box>
+    );
 }
 
 export default Colaboradores
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,60 +1,98 @@
 import './Home.css'
-import { Box, Container, Typography } from '@mui/material';
+import { Box, Container, Typography, Button } from '@mui/material';
 import backgroundImage from './../assets/pexels-fauxels-3184424.jpg'; 
 
 const Home = () => {
 
     return (
         <Box 
-        sx={{
-            position: 'relative',
-            height: '100vh', // Ajusta la altura según tus necesidades
-            color: 'white',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }}
-    >
-        {/* Overlay con efecto multiply */}
-        <Box 
             sx={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundColor: 'rgba(0, 0, 128, 0.5)', // Color azul marino con opacidad
-                mixBlendMode: 'multiply', // Efecto multiply
-                zIndex: 1
+                position: 'relative',
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center'
             }}
-        />
+        >
+            {/* Overlay muy oscuro (azul casi negro) */}
+            <Box 
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundColor: 'rgba(0, 0, 50, 0.75)', // Azul muy oscuro, casi negro
+                    mixBlendMode: 'multiply',
+                    zIndex: 1
+                }}
+            />
 
-        {/* Contenido */}
-        <Container maxWidth="sm" sx={{ position: 'relative', zIndex: 2 }}>
-            <Typography variant="h2" component="h1" gutterBottom>
-                Encode Enterprise
-            </Typography>
-            <Typography variant="subtitle1">
-                En Encode Enterprise, fusionamos innovación y experiencia para entregar soluciones de software sobresalientes...
-            </Typography>
-        </Container>
+            {/* Contenido */}
+            <Container maxWidth="sm" sx={{ position: 'relative', zIndex: 2, textAlign: 'center' }}>
+                <Typography 
+                    variant="h2" 
+                    component="h1" 
+                    gutterBottom
+                    sx={{
+                        fontFamily: 'Poppins, sans-serif',
+                        fontWeight: 700,
+                        background: 'linear-gradient(to right, #007bff, #ff69b4)', // Degradado de azul a rosa
+                        WebkitBackgroundClip: 'text',
+                        WebkitTextFillColor: 'transparent',
+                        backgroundClip: 'text',
+                        textFillColor: 'transparent'
+                    }}
+                >
+                    Encode Enterprise
+                </Typography>
+                <Typography 
+                    variant="subtitle1" 
+                    sx={{ 
+                        fontFamily: 'Poppins, sans-serif',
+                        color: 'white' // Asegurando que el texto sea blanco
+                    }}
+                >
+                    Encode Enterprise es una empresa de desarrollo de software dedicada a ofrecer soluciones personalizadas que impulsen la innovación y la eficiencia en su organización. 
+                        </Typography>
 
-        {/* Imagen de fondo */}
-        <Box 
-            sx={{
-                position: 'absolute',
-                top: 0,
-                left: 0,
-                right: 0,
-                bottom: 0,
-                backgroundImage: `url(${backgroundImage})`,
-                backgroundSize: 'cover',
-                backgroundPosition: 'center',
-                zIndex: 0
-            }}
-        />
-    </Box>
+            {/* Botón de contacto */}
+            <Button 
+                    variant="contained" 
+                    sx={{
+                        mt: 4,
+                        fontFamily: 'Poppins, sans-serif',
+                        fontWeight: 700,
+                        borderRadius: '15px',
+                        background: 'linear-gradient(45deg, #007bff, #ff69b4)',
+                        color: 'black',
+                        padding: '10px 30px',
+                        '&:hover': {
+                            background: 'linear-gradient(45deg, #0062cc, #ff1493)',
+                        },
+                    }}
+                >
+                    Contáctanos
+                </Button>
+        
+            </Container>
+
+            {/* Imagen de fondo */}
+            <Box 
+                sx={{
+                    position: 'absolute',
+                    top: 0,
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    backgroundImage: `url(${backgroundImage})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    zIndex: 0
+                }}
+            />
+        </Box>
     )
 }
 
